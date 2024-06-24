7d2d2415100499357125250797c9e78b3dd658c9
MrSony <ignatenkov02@gmail.com>
2024-06-24 23:40:16 +0300

    Избавился от lombok fix tests typo поправил не правильный ответ на правильный

--- app/src/androidTest/java/com/mrsony/idontbelieve/QuestionApplicationTests.java
+++ app/src/androidTest/java/com/mrsony/idontbelieve/QuestionApplicationTests.java
@@ -1,16 +1,11 @@
 package com.mrsony.idontbelieve;
 
-import static com.adevinta.android.barista.assertion.BaristaContentDescriptionAssertions.assertContentDescription;
 import static com.adevinta.android.barista.assertion.BaristaVisibilityAssertions.assertContains;
 import static com.adevinta.android.barista.assertion.BaristaVisibilityAssertions.assertDisplayed;
 import static com.adevinta.android.barista.interaction.BaristaClickInteractions.clickOn;
 import static com.adevinta.android.barista.interaction.BaristaClickInteractions.longClickOn;
 import static com.adevinta.android.barista.interaction.BaristaSleepInteractions.sleep;
-import static com.google.android.apps.common.testing.accessibility.framework.strings.StringManager.getString;
 
-import android.content.Context;
-
-import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
 
@@ -18,15 +13,8 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
-
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
 @RunWith(AndroidJUnit4.class)
-public class ExampleInstrumentedTest {
+public class QuestionApplicationTests {
     @Rule
     public ActivityTestRule<MainActivity> activityRule = new ActivityTestRule<>(MainActivity.class);
 
@@ -41,7 +29,7 @@ public class ExampleInstrumentedTest {
 
         int i = 1;
         for (boolean answ : answers) {
-            Question("Утверждение #" + i, answ);
+            checkQuestion("Утверждение #" + i, answ);
             i++;
         }
         assertDisplayed(R.id.gameResultView);
@@ -52,18 +40,11 @@ public class ExampleInstrumentedTest {
         sleep(4000);
     }
 
-    public void Question(String text, boolean answer) {
+    private void checkQuestion(String text, boolean answer) {
         sleep(4000);
         assertContains(text);
         longClickOn(answer ? R.id.believe_btn : R.id.dont_believe_btn);
         assertDisplayed(R.string.right);
         sleep(3600);
-    }
-
-    @Test
-    public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.mrsony.idontbelieve", appContext.getPackageName());
     }
 }
--- app/src/main/java/com/mrsony/idontbelieve/models/Question.java
+++ app/src/main/java/com/mrsony/idontbelieve/models/Question.java
@@ -1,13 +1,31 @@
 package com.mrsony.idontbelieve.models;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-@Getter
-@AllArgsConstructor
 public class Question {
-    private int id;
-    private String statement;
-    private boolean correctAnswer;
-    private String justification;
+    private final int id;
+    private final String statement;
+    private final boolean correctAnswer;
+    private final String justification;
+
+    public Question(int id, String statement, boolean correctAnswer, String justification) {
+        this.id = id;
+        this.statement = statement;
+        this.correctAnswer = correctAnswer;
+        this.justification = justification;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getStatement() {
+        return statement;
+    }
+
+    public boolean isCorrectAnswer() {
+        return correctAnswer;
+    }
+
+    public String getJustification() {
+        return justification;
+    }
 }
